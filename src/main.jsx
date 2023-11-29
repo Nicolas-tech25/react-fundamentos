585ed2a1d5e81e450be6ffb6267b6319bf217596
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-11-29 10:53:04 -0300

    uso de fragmentos

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 /*  Importaçãodo primeiro/principal componente, arquivo em que carregamos os elementos da pagina */
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
