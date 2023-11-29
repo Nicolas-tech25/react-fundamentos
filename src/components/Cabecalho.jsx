b10a3d13ffe002c084db256cd05b84da308f6163
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-11-29 11:56:16 -0300

    components

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,18 @@
+function Cabecalho() {
+  return (
+    <header>
+      <h1>Olá React! 🎈</h1>
+
+      <hr />
+
+      <nav>
+        <a href="">Home</a>
+        <a href="">Produtos</a>
+        <a href="">Contato</a>
+      </nav>
+    </header>
+    /* Comentário no meio de JS é normal ficar entre { nesta situação} */
+  );
+}
+
+export default Cabecalho;
---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,33 @@
+function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2>Conteúdo da aplicação</h2>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          atque odit tenetur. Veritatis architecto dolorem voluptatem, porro
+          optio repudiandae odit a! Nihil nam laudantium rem facere, distinctio
+          ratione quas possimus fugit soluta harum, alias repellat?
+        </p>
+
+        <article>
+          <h3>Artigo 1</h3>
+          <p>Conteúdo do artigo 1....</p>
+        </article>
+
+        <article>
+          <h3>Artigo 2</h3>
+          <p>Conteúdo do artigo 2....</p>
+        </article>
+
+        <article>
+          <h3>Artigo 3</h3>
+          <p>Conteúdo do artigo 3....</p>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default Conteudo;
