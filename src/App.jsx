2ab2beb52202ab47ff23494b5845bb3aeb521495
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-11-29 11:10:51 -0300

    formatação com prittier

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ function App() {
   return <>
   <header>
     <h1>Olá React! 🎈</h1>
+    <hr />
+    <nav>
+      <a href="">Home</a>
+      <a href="">Produtos</a>
+      <a href="">Contato</a>
+    </nav>
   </header>
+  {/* Comentário no meio de JS é normal ficar entre { nesta situação} */}
   <main>
-    <h2>VAI CORINTHIANS 🦅🦅</h2>
+    <h2>VAI CORINTHIANS <span>🦅🦅</span></h2>
   </main>
 </>
 }
