0c7ae3315b5243fbf60ac66658ddfb4cadf88ff3
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-11-30 09:03:48 -0300

    componente menu

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,15 +1,12 @@
+import Menu from "./Menu";
+
 function Cabecalho() {
   return (
     <header>
       <h1>Olá React! 🎈</h1>
 
+      <Menu />
       <hr />
-
-      <nav>
-        <a href="">Home</a>
-        <a href="">Produtos</a>
-        <a href="">Contato</a>
-      </nav>
     </header>
     /* Comentário no meio de JS é normal ficar entre { nesta situação} */
   );
---
+++ src/components/Menu.jsx
@@ -0,0 +1,10 @@
+function Menu() {
+  return (
+    <nav>
+      <a href="">Home</a>
+      <a href="">Produtos</a>
+      <a href="">Contato</a>
+    </nav>
+  );
+}
+export default Menu;
