e4b1447ee3e3c6bcdf2afb236dcb9c01dbef1ea9
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-12-01 09:55:25 -0300

    css

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,23 +1,36 @@
 import Artigo from "./Artigo";
 
+import styled from "styled-components";
+
+const StyledConteudo = styled.main`
+  width: 90vw;
+  margin: 1rem auto;
+  background-color: aliceblue;
+  padding: 1rem;
+  box-shadow: darkblue 0 0 1px inset;
+
+  h2,
+  p {
+    padding: 0.2rem 0;
+  }
+`;
+
 function Conteudo() {
   return (
-    <main>
-      <section>
-        <h2>Conteúdo da aplicação</h2>
+    <StyledConteudo>
+      <h2>Conteúdo da aplicação</h2>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-          atque odit tenetur. Veritatis architecto dolorem voluptatem, porro
-          optio repudiandae odit a! Nihil nam laudantium rem facere, distinctio
-          ratione quas possimus fugit soluta harum, alias repellat?
-        </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus atque
+        odit tenetur. Veritatis architecto dolorem voluptatem, porro optio
+        repudiandae odit a! Nihil nam laudantium rem facere, distinctio ratione
+        quas possimus fugit soluta harum, alias repellat?
+      </p>
 
-        <Artigo />
-        <Artigo />
-        <Artigo />
-      </section>
-    </main>
+      <Artigo />
+      <Artigo />
+      <Artigo />
+    </StyledConteudo>
   );
 }
 
